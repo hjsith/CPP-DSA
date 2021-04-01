d4fd50262ab606466dc4d463df40522cd0349ac2
hjsith <hasithjayasekera@gmail.com>
2021-04-02 01:08:47 +1100

    Added main functions to view sorting algorithms

---
+++ Algorithms/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include "sorts.h"
+
+int main()
+{
+    std::vector<int> v = {92, 85, 17, 8, 80, 76, 88, 6, 57, 79, 33, 58, 1, 9, 20, 24, 10, 62, 42, 15, 46, 53, 83, 52, 19};
+
+    std::cout << "######################### INSERTION SORT #########################" << std::endl;
+    std::cout << "------------------------------------------------------------------" << std::endl;
+    for (auto i = v.begin(); i != v.end(); ++i) std::cout << *i << ' ';
+    std::cout << std::endl;
+
+    std::vector<int> v_ans = sort::insertion_sort(v);
+
+    for (auto i = v_ans.begin(); i != v_ans.end(); ++i) std::cout << *i << ' ';
+    std::cout << std::endl;
+
+    std::cout << "------------------------------------------------------------------" << std::endl;
+
+}
