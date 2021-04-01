3233a8d4ecc2ab111e0afff532a57dab72a08c6b
hjsith <hasithjayasekera@gmail.com>
2021-04-01 17:06:02 +1100

    Add insertion sort method and empty test for it

---
+++ Algorithms/sorts.h
@@ -0,0 +1,22 @@
+#pragma once
+#define sorts
+
+#include <vector>
+
+namespace sort
+{
+    std::vector<int> insertion_sort(std::vector<int> data)
+    {
+        for (int j = 1; j < data.size(); ++j)
+        {
+            int value = data[j];
+            auto i = j - 1;
+            while (i >= 0 && data[i] > value)
+            {
+                data[i + 1] = data[i];
+                i = i - 1;
+            }
+            data[i + 1] = value;
+        }
+    }
+}
